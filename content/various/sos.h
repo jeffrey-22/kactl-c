e6b9cd90534de5e6a7d5b4ae2118134aa8e86bb1
Jiayou Song <--replace-all>
2023-10-15 13:54:24 +0100

    fix sos

--- content/various/sos.h
+++ content/various/sos.h
@@ -3,11 +3,14 @@
  * Date: 2009-10-26
  * License: CC0
  * Source: folklore
- * Description: Sum over subsets
- * Time: $O(3^n)$
+ * Description: Sum over subsets, F[mask] is sum of A[x] such that x is a subset of mask
+ * Time: $O(n\cdot 2^n)$
  */
 #pragma once
 
-for (int i = 0; i < (1 << n); i++)
-	// iterate over all subsets of i directly
-	for (int j = (i - 1) & i; j >= 0; j = (j - 1) & i) sos[i] += a[j];
+for(int i = 0; i<(1<<N); ++i)
+	F[i] = A[i];
+    for(int i = 0;i < N; ++i) for(int mask = 0; mask < (1<<N); ++mask){
+        if(mask & (1<<i))
+            F[mask] += F[mask^(1<<i)];
+}
